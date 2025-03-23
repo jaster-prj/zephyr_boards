3fe510e1af43a0e53b87d236793fcb16419d4848
Tobias Jaster <tobias.jaster@posteo.de>
2025-03-23 21:25:10 +0100

    Update link.sh

--- link.sh
+++ link.sh
@@ -3,5 +3,8 @@
 path=$(dirname $(realpath "$0"))
 echo $path
 for d in */ ; do
-    ln -s "$path/$d" ../boards/
+    if [ -e "../boards/$d"];then
+        rm -rf "../boards/$d"
+    fi
+    cp -r "$path/$d" ../boards/
 done
