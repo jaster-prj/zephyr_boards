12f15ab61ebaffd3e2f7fa5fe26bdeb9522420ae
Tobias Jaster <tobias.jaster@posteo.de>
2025-03-30 20:51:50 +0200

    Fix link script

--- link.sh
+++ link.sh
@@ -3,7 +3,7 @@
 path=$(dirname $(realpath "$0"))
 echo $path
 for d in */ ; do
-    if [ -e "../boards/$d"];then
+    if [ -e "../boards/$d" ];then
         rm -rf "../boards/$d"
     fi
     cp -r "$path/$d" ../boards/
